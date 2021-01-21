590bb108b04743ec4cc2e7263e0c77cd0dc25c37
Addison Martin <addisonmartin@hey.com>
2021-01-21 12:35:57 -0500

    Posting data to remote API.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // Immediately create and run GPSLoggingAndMapping app.
 void main() {
@@ -35,6 +37,8 @@ class _UserLocationState extends State<UserLocation> {
   final _databaseName = 'gps_logging_and_mapping_app.db';
   final _databaseMigrationVersion = 1;
   Database _database; // Store the history to a local database, for later syncing to the server.
+
+  final _remoteAPIURL = 'http://10.0.2.2:3000/points/'; // 10.0.2.2 == localhost for the android emulator.
 
   final _biggerFont = TextStyle(fontSize: 18.0);
   final _listViewScrollController = new ScrollController();
@@ -120,30 +124,57 @@ class _UserLocationState extends State<UserLocation> {
       _currentUserLocation = newLocation;
       _userLocationHistory.add(_currentUserLocation);
     });
-    saveRecordToDatabase(_currentUserLocation);
+    _saveRecordToDatabase(_currentUserLocation);
+    _postRecordToRemoteAPI(_currentUserLocation);
   }
 
   void _setupDatabase() async {
     _database = await openDatabase(_databaseName, version: _databaseMigrationVersion,
         onCreate: (Database db, int version) async {
-          // When creating the database, create the table's for points.
-          if (version == 1) {
-            await db.execute(
-                'CREATE TABLE points (id INTEGER PRIMARY KEY, name TEXT, comment TEXT, latitude REAL, longitude REAL, accuracy REAL, altitude REAL, speed REAL, heading REAL, recorded_at TEXT, point_of_interest INTEGER)'
-            );
-          }
+      // When creating the database, create the table's for points.
+      if (version == 1) {
+        await db.execute(
+            'CREATE TABLE points (id INTEGER PRIMARY KEY, name TEXT, comment TEXT, latitude REAL, longitude REAL, accuracy REAL, altitude REAL, speed REAL, heading REAL, recorded_at TEXT, point_of_interest INTEGER)');
       }
-    );
+    });
   }
 
-  void saveRecordToDatabase(LocationData point) {
+  void _saveRecordToDatabase(LocationData point) {
     _database.transaction((txn) async {
-      var record = await txn.rawInsert('INSERT INTO points(name, latitude, longitude, accuracy, altitude, speed, heading, recorded_at, point_of_interest) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                    ['point name *', point.latitude, point.longitude, point.accuracy, point.altitude, point.speed, point.heading, _convertToDateTime(point.time.toInt()), 0]);
+      var record = await txn.rawInsert(
+          'INSERT INTO points(name, latitude, longitude, accuracy, altitude, speed, heading, recorded_at, point_of_interest) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+          [
+            'point name *',
+            point.latitude,
+            point.longitude,
+            point.accuracy,
+            point.altitude,
+            point.speed,
+            point.heading,
+            _convertToDateTime(point.time.toInt()),
+            0
+          ]);
     });
   }
 
   String _convertToDateTime(int time) {
     return DateTime.fromMillisecondsSinceEpoch(time).toString();
+  }
+
+  void _postRecordToRemoteAPI(LocationData point) async {
+    var response = await http.post(_remoteAPIURL, body: jsonEncode({'point': {
+      'name': 'idk test name?',
+      'latitude': point.latitude.toString(),
+      'longitude': point.longitude.toString(),
+      'accuracy': point.accuracy.toString(),
+      'altitude': point.altitude.toString(),
+      'speed': point.speed.toString(),
+      'heading': point.heading.toString(),
+      'recorded_at': _convertToDateTime(point.time.toInt()),
+      'point_of_interest': "0"
+    }}));
+
+    print('Response Status: ${response.statusCode}');
+    print('Response Body: ${response.body}');
   }
 }
